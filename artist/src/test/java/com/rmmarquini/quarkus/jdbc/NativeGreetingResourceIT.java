d277065d0170b2aa1a5be20607d7a5f622d6e83e
Rafa Marquini <rafa.mardegan@gmail.com>
2022-01-08 14:06:22 -0300

    Initial commit

---
+++ artist/src/test/java/com/rmmarquini/quarkus/jdbc/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.rmmarquini.quarkus.jdbc;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
